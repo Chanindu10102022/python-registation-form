ced7cbdda41351c0cadac897133692e4f35f6fa2
Chanindu10102022 <103425107+Chanindu10102022@users.noreply.github.com>
2022-04-11 17:11:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+# name
+
+name = input("what is you're name? ")
+
+# age
+
+age = input("what is you're age? ")
+
+# id no:
+
+id_no = input("what is you're identicard number? ")
+
+# Gender
+
+print("use true or false")
+
+Gender = print("what is you're Gender?")
+
+ask = input("Male? ")
+ask2 = input("Femail? ")
+
+
+if name:
+    print("")
+else:
+    print("Can't identify who are you..")
+
+if age:
+    print("")
+else:
+    print("Can't identify you're age..")
+
+if id_no:
+    print("")
+else:
+    print(" can't identify you're idNO ")
